81cd4907d9df0b18a3a51c88d13f2b02c71f1a23
Yu <miller2017@qq.com>
2023-12-08 15:18:30 +0800

    Update Kotlin support

--- serialport/src/main/kotlin/android/serialport/SerialPort.kt
+++ serialport/src/main/kotlin/android/serialport/SerialPort.kt
@@ -13,236 +13,115 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package android.serialport
 
-package android.serialport;
+import android.util.Log
+import java.io.*
 
-import android.util.Log;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public final class SerialPort {
-
-    private static final String TAG = "SerialPort";
-
-    public static final String DEFAULT_SU_PATH = "/system/bin/su";
-
-    private static String sSuPath = DEFAULT_SU_PATH;
-    private File device;
-    private int baudrate;
-    private int dataBits;
-    private int parity;
-    private int stopBits;
-    private int flags;
-
-    /**
-     * Set the su binary path, the default su binary path is {@link #DEFAULT_SU_PATH}
-     *
-     * @param suPath su binary path
-     */
-    public static void setSuPath(@Nullable String suPath) {
-        if (suPath == null) {
-            return;
-        }
-        sSuPath = suPath;
-    }
-
-    /**
-     * Get the su binary path
-     *
-     * @return
-     */
-    @NonNull
-    public static String getSuPath() {
-        return sSuPath;
-    }
+/**
+ * 串口
+ *
+ * @param device 串口设备文件
+ * @param baudrate 波特率
+ * @param dataBits 数据位；默认8,可选值为5~8
+ * @param parity 奇偶校验；0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN)
+ * @param stopBits 停止位；默认1；1:1位停止位；2:2位停止位
+ * @param flags 默认0
+ * @throws SecurityException
+ * @throws IOException
+ */
+class SerialPort @JvmOverloads constructor(
+    /** 串口设备文件  */
+    private val device: File,
+    /** 波特率  */
+    private val baudrate: Int,
+    /** 数据位；默认8,可选值为5~8  */
+    private val dataBits: Int = 8,
+    /** 奇偶校验；0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN)  */
+    private val parity: Int = 0,
+    /** 停止位；默认1；1:1位停止位；2:2位停止位  */
+    private val stopBits: Int = 1,
+    private val flags: Int = 0
+) {
 
     /*
-     * Do not remove or rename the field mFd: it is used by native method close();
-     */
-    private FileDescriptor mFd;
-    private FileInputStream mFileInputStream;
-    private FileOutputStream mFileOutputStream;
-
-    /**
-     * 串口
-     *
-     * @param device 串口设备文件
-     * @param baudrate 波特率
-     * @param dataBits 数据位；默认8,可选值为5~8
-     * @param parity 奇偶校验；0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN)
-     * @param stopBits 停止位；默认1；1:1位停止位；2:2位停止位
-     * @param flags 默认0
-     * @throws SecurityException
-     * @throws IOException
-     */
-    public SerialPort(@NonNull File device, int baudrate, int dataBits, int parity, int stopBits,
-        int flags) throws SecurityException, IOException {
-
-        this.device = device;
-        this.baudrate = baudrate;
-        this.dataBits = dataBits;
-        this.parity = parity;
-        this.stopBits = stopBits;
-        this.flags = flags;
-
-        /* Check access permission */
-        if (!device.canRead() || !device.canWrite()) {
-            try {
-                /* Missing read/write permission, trying to chmod the file */
-                Process su;
-                su = Runtime.getRuntime().exec(sSuPath);
-                String cmd = "chmod 666 " + device.getAbsolutePath() + "\n" + "exit\n";
-                su.getOutputStream().write(cmd.getBytes());
-                if ((su.waitFor() != 0) || !device.canRead() || !device.canWrite()) {
-                    throw new SecurityException();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new SecurityException();
-            }
-        }
-
-        mFd = open(device.getAbsolutePath(), baudrate, dataBits, parity, stopBits, flags);
-        if (mFd == null) {
-            Log.e(TAG, "native open returns null");
-            throw new IOException();
-        }
-        mFileInputStream = new FileInputStream(mFd);
-        mFileOutputStream = new FileOutputStream(mFd);
-    }
-
-    /**
-     * 串口，默认的8n1
-     *
-     * @param device 串口设备文件
-     * @param baudrate 波特率
-     * @throws SecurityException
-     * @throws IOException
-     */
-    public SerialPort(@NonNull File device, int baudrate) throws SecurityException, IOException {
-        this(device, baudrate, 8, 0, 1, 0);
-    }
-
-    /**
-     * 串口
-     *
-     * @param device 串口设备文件
-     * @param baudrate 波特率
-     * @param dataBits 数据位；默认8,可选值为5~8
-     * @param parity 奇偶校验；0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN)
-     * @param stopBits 停止位；默认1；1:1位停止位；2:2位停止位
-     * @throws SecurityException
-     * @throws IOException
-     */
-    public SerialPort(@NonNull File device, int baudrate, int dataBits, int parity, int stopBits)
-        throws SecurityException, IOException {
-        this(device, baudrate, dataBits, parity, stopBits, 0);
-    }
-
-    // Getters and setters
-    @NonNull
-    public InputStream getInputStream() {
-        return mFileInputStream;
-    }
-
-    @NonNull
-    public OutputStream getOutputStream() {
-        return mFileOutputStream;
-    }
-
-    /** 串口设备文件 */
-    @NonNull
-    public File getDevice() {
-        return device;
-    }
-
-    /** 波特率 */
-    public int getBaudrate() {
-        return baudrate;
-    }
-
-    /** 数据位；默认8,可选值为5~8 */
-    public int getDataBits() {
-        return dataBits;
-    }
-
-    /** 奇偶校验；0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN) */
-    public int getParity() {
-        return parity;
-    }
-
-    /** 停止位；默认1；1:1位停止位；2:2位停止位 */
-    public int getStopBits() {
-        return stopBits;
-    }
-
-    public int getFlags() {
-        return flags;
-    }
+       * Do not remove or rename the field mFd: it is used by native method close();
+       */
+    private val mFd: FileDescriptor?
+    private val mFileInputStream: FileInputStream
+    private val mFileOutputStream: FileOutputStream
+    val inputStream: InputStream
+        // Getters and setters
+        get() = mFileInputStream
+    val outputStream: OutputStream
+        get() = mFileOutputStream
 
     // JNI
-    private native FileDescriptor open(String absolutePath, int baudrate, int dataBits, int parity,
-        int stopBits, int flags);
+    private external fun open(
+        absolutePath: String, baudrate: Int, dataBits: Int, parity: Int,
+        stopBits: Int, flags: Int
+    ): FileDescriptor?
 
-    public native void close();
+    external fun close()
 
-    /** 关闭流和串口，已经try-catch */
-    public void tryClose() {
+    /** 关闭流和串口，已经try-catch  */
+    fun tryClose() {
         try {
-            mFileInputStream.close();
-        } catch (IOException e) {
+            mFileInputStream.close()
+        } catch (e: IOException) {
             //e.printStackTrace();
         }
-
         try {
-            mFileOutputStream.close();
-        } catch (IOException e) {
+            mFileOutputStream.close()
+        } catch (e: IOException) {
             //e.printStackTrace();
         }
-
         try {
-            close();
-        } catch (Exception e) {
+            close()
+        } catch (e: Exception) {
             //e.printStackTrace();
         }
     }
 
-    static {
-        System.loadLibrary("serial_port");
-    }
+    init {
 
-    public static Builder newBuilder(File device, int baudrate) {
-        return new Builder(device, baudrate);
-    }
-
-    public static Builder newBuilder(String devicePath, int baudrate) {
-        return new Builder(devicePath, baudrate);
-    }
-
-    public final static class Builder {
-
-        private File device;
-        private int baudrate;
-        private int dataBits = 8;
-        private int parity = 0;
-        private int stopBits = 1;
-        private int flags = 0;
-
-        private Builder(File device, int baudrate) {
-            this.device = device;
-            this.baudrate = baudrate;
+        /* Check access permission */if (!device.canRead() || !device.canWrite()) {
+            try {
+                /* Missing read/write permission, trying to chmod the file */
+                val su: Process = Runtime.getRuntime().exec(sSuPath)
+                val cmd = """
+                    chmod 666 ${device.absolutePath}
+                    exit
+                    
+                    """.trimIndent()
+                su.outputStream.write(cmd.toByteArray())
+                if (su.waitFor() != 0 || !device.canRead() || !device.canWrite()) {
+                    throw SecurityException()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                throw SecurityException()
+            }
         }
-
-        private Builder(String devicePath, int baudrate) {
-            this(new File(devicePath), baudrate);
+        mFd = open(device.absolutePath, baudrate, dataBits, parity, stopBits, flags)
+        if (mFd == null) {
+            Log.e(TAG, "native open returns null")
+            throw IOException()
         }
+        mFileInputStream = FileInputStream(mFd)
+        mFileOutputStream = FileOutputStream(mFd)
+    }
+
+    class Builder(
+        private val device: File,
+        private val baudrate: Int
+    ) {
+        private var dataBits = 8
+        private var parity = 0
+        private var stopBits = 1
+        private var flags = 0
+
+        constructor(devicePath: String, baudrate: Int) : this(File(devicePath), baudrate)
 
         /**
          * 数据位
@@ -250,9 +129,9 @@ public final class SerialPort {
          * @param dataBits 默认8,可选值为5~8
          * @return
          */
-        public Builder dataBits(int dataBits) {
-            this.dataBits = dataBits;
-            return this;
+        fun dataBits(dataBits: Int): Builder {
+            this.dataBits = dataBits
+            return this
         }
 
         /**
@@ -261,9 +140,9 @@ public final class SerialPort {
          * @param parity 0:无校验位(NONE，默认)；1:奇校验位(ODD);2:偶校验位(EVEN)
          * @return
          */
-        public Builder parity(int parity) {
-            this.parity = parity;
-            return this;
+        fun parity(parity: Int): Builder {
+            this.parity = parity
+            return this
         }
 
         /**
@@ -272,9 +151,9 @@ public final class SerialPort {
          * @param stopBits 默认1；1:1位停止位；2:2位停止位
          * @return
          */
-        public Builder stopBits(int stopBits) {
-            this.stopBits = stopBits;
-            return this;
+        fun stopBits(stopBits: Int): Builder {
+            this.stopBits = stopBits
+            return this
         }
 
         /**
@@ -283,9 +162,9 @@ public final class SerialPort {
          * @param flags 默认0
          * @return
          */
-        public Builder flags(int flags) {
-            this.flags = flags;
-            return this;
+        fun flags(flags: Int): Builder {
+            this.flags = flags
+            return this
         }
 
         /**
@@ -295,8 +174,46 @@ public final class SerialPort {
          * @throws SecurityException
          * @throws IOException
          */
-        public SerialPort build() throws SecurityException, IOException {
-            return new SerialPort(device, baudrate, dataBits, parity, stopBits, flags);
+        @Throws(SecurityException::class, IOException::class)
+        fun build(): SerialPort {
+            return SerialPort(device, baudrate, dataBits, parity, stopBits, flags)
+        }
+    }
+
+    companion object {
+        private const val TAG = "SerialPort"
+        private const val DEFAULT_SU_PATH = "/system/bin/su"
+        private var sSuPath = DEFAULT_SU_PATH
+        var suPath: String?
+            /**
+             * Get the su binary path
+             *
+             * @return
+             */
+            get() = sSuPath
+            /**
+             * Set the su binary path, the default su binary path is [.DEFAULT_SU_PATH]
+             *
+             * @param suPath su binary path
+             */
+            set(suPath) {
+                if (suPath == null) {
+                    return
+                }
+                sSuPath = suPath
+            }
+
+        init {
+            System.loadLibrary("serial_port")
+        }
+
+        fun newBuilder(device: File, baudrate: Int): Builder {
+            return Builder(device, baudrate)
+        }
+
+        @JvmStatic
+        fun newBuilder(devicePath: String, baudrate: Int): Builder {
+            return Builder(devicePath, baudrate)
         }
     }
 }
--- serialport/src/main/kotlin/android/serialport/SerialPortFinder.kt
+++ serialport/src/main/kotlin/android/serialport/SerialPortFinder.kt
@@ -13,116 +13,106 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
+package android.serialport
 
-package android.serialport;
+import android.util.Log
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
+import java.io.LineNumberReader
+import java.util.Vector
 
-import android.util.Log;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.util.Iterator;
-import java.util.Vector;
-
-public class SerialPortFinder {
-
-    public class Driver {
-        public Driver(String name, String root) {
-            mDriverName = name;
-            mDeviceRoot = root;
-        }
-
-        private String mDriverName;
-        private String mDeviceRoot;
-        Vector<File> mDevices = null;
-
-        public Vector<File> getDevices() {
-            if (mDevices == null) {
-                mDevices = new Vector<File>();
-                File dev = new File("/dev");
-                
-                File[] files = dev.listFiles();
-
-                if (files != null) {
-                    int i;
-                    for (i = 0; i < files.length; i++) {
-                        if (files[i].getAbsolutePath().startsWith(mDeviceRoot)) {
-                            Log.d(TAG, "Found new device: " + files[i]);
-                            mDevices.add(files[i]);
+class SerialPortFinder {
+    inner class Driver(val name: String, private val mDeviceRoot: String) {
+        var mDevices: Vector<File>? = null
+        val devices: Vector<File>
+            get() {
+                if (mDevices == null) {
+                    mDevices = Vector()
+                    val dev = File("/dev")
+                    val files = dev.listFiles()
+                    if (files != null) {
+                        var i: Int
+                        i = 0
+                        while (i < files.size) {
+                            if (files[i].absolutePath.startsWith(mDeviceRoot)) {
+                                Log.d(TAG, "Found new device: " + files[i])
+                                mDevices!!.add(files[i])
+                            }
+                            i++
                         }
                     }
                 }
+                return mDevices!!
             }
-            return mDevices;
-        }
-
-        public String getName() {
-            return mDriverName;
-        }
     }
 
-    private static final String TAG = "SerialPort";
+    private var mDrivers: Vector<Driver>? = null
 
-    private Vector<Driver> mDrivers = null;
-
-    Vector<Driver> getDrivers() throws IOException {
-        if (mDrivers == null) {
-            mDrivers = new Vector<Driver>();
-            LineNumberReader r = new LineNumberReader(new FileReader("/proc/tty/drivers"));
-            String l;
-            while ((l = r.readLine()) != null) {
-                // Issue 3:
-                // Since driver name may contain spaces, we do not extract driver name with split()
-                String drivername = l.substring(0, 0x15).trim();
-                String[] w = l.split(" +");
-                if ((w.length >= 5) && (w[w.length - 1].equals("serial"))) {
-                    Log.d(TAG, "Found new driver " + drivername + " on " + w[w.length - 4]);
-                    mDrivers.add(new Driver(drivername, w[w.length - 4]));
+    @get:Throws(IOException::class)
+    val drivers: Vector<Driver>
+        get() {
+            if (mDrivers == null) {
+                mDrivers = Vector()
+                val r = LineNumberReader(FileReader("/proc/tty/drivers"))
+                var l: String
+                while (r.readLine().also { l = it } != null) {
+                    // Issue 3:
+                    // Since driver name may contain spaces, we do not extract driver name with split()
+                    val drivername = l.substring(0, 0x15).trim { it <= ' ' }
+                    val w = l.split(" +".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                    if (w.size >= 5 && w[w.size - 1] == "serial") {
+                        Log.d(TAG, "Found new driver " + drivername + " on " + w[w.size - 4])
+                        mDrivers!!.add(Driver(drivername, w[w.size - 4]))
+                    }
                 }
+                r.close()
             }
-            r.close();
+            return mDrivers!!
         }
-        return mDrivers;
-    }
-
-    public String[] getAllDevices() {
-        Vector<String> devices = new Vector<String>();
-        // Parse each driver
-        Iterator<Driver> itdriv;
-        try {
-            itdriv = getDrivers().iterator();
-            while (itdriv.hasNext()) {
-                Driver driver = itdriv.next();
-                Iterator<File> itdev = driver.getDevices().iterator();
-                while (itdev.hasNext()) {
-                    String device = itdev.next().getName();
-                    String value = String.format("%s (%s)", device, driver.getName());
-                    devices.add(value);
+    val allDevices: Array<String>
+        get() {
+            val devices = Vector<String>()
+            // Parse each driver
+            val itdriv: Iterator<Driver>
+            try {
+                itdriv = drivers.iterator()
+                while (itdriv.hasNext()) {
+                    val driver = itdriv.next()
+                    val itdev: Iterator<File> = driver.devices.iterator()
+                    while (itdev.hasNext()) {
+                        val device = itdev.next().name
+                        val value = String.format("%s (%s)", device, driver.name)
+                        devices.add(value)
+                    }
                 }
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            return devices.toTypedArray()
         }
-        return devices.toArray(new String[devices.size()]);
-    }
-
-    public String[] getAllDevicesPath() {
-        Vector<String> devices = new Vector<String>();
-        // Parse each driver
-        Iterator<Driver> itdriv;
-        try {
-            itdriv = getDrivers().iterator();
-            while (itdriv.hasNext()) {
-                Driver driver = itdriv.next();
-                Iterator<File> itdev = driver.getDevices().iterator();
-                while (itdev.hasNext()) {
-                    String device = itdev.next().getAbsolutePath();
-                    devices.add(device);
+    val allDevicesPath: Array<String>
+        get() {
+            val devices = Vector<String>()
+            // Parse each driver
+            val itdriv: Iterator<Driver>
+            try {
+                itdriv = drivers.iterator()
+                while (itdriv.hasNext()) {
+                    val driver = itdriv.next()
+                    val itdev: Iterator<File> = driver.devices.iterator()
+                    while (itdev.hasNext()) {
+                        val device = itdev.next().absolutePath
+                        devices.add(device)
+                    }
                 }
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            return devices.toTypedArray()
         }
-        return devices.toArray(new String[devices.size()]);
+
+    companion object {
+        private const val TAG = "SerialPort"
     }
 }
